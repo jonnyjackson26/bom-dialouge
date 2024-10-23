d5afba764d23b7f037b072a1034eaf2e248576b1
Jonny Jackson <jrsjackson26@gmail.com>
2024-10-23 10:48:10 -0600

    added google sheets database functionality

--- src/Pages/ChapterPage/ChapterPage.jsx
+++ src/Pages/ChapterPage/ChapterPage.jsx
@@ -7,9 +7,13 @@ import { getNextButtonInfo, getPrevButtonInfo } from "../../utils/next-and-prev-
 import books from "../../../public/data/books.js"
 import "../ChapterPage/ChapterPage.css"
 
+//google sheets database 
+import axios from 'axios';
+import { google_api_key, sheet_id } from '../../../api_keys';
 
 export function ChapterPage({ book, chapter, setSelectedChapter, setSelectedBook }) {
     const [verses, setVerses] = useState([]);
+    const [dialougeInfo, setDialougeInfo] = useState([]);
     
     DocumentTitle(book.urlName + " " + chapter);
 
@@ -33,6 +37,27 @@ export function ChapterPage({ book, chapter, setSelectedChapter, setSelectedBook
         fetchVerses();
     }, [book.urlName, chapter]);
 
+    useEffect(() => {
+        const fetchDialouge = async () => {
+            const sheetId = sheet_id;
+            const apiKey = google_api_key;
+            const range = "Sheet1!A1:C1"; // Fetching B1 to B11
+    
+            const response = await axios.get(
+                `https://sheets.googleapis.com/v4/spreadsheets/${sheetId}/values/${range}?key=${apiKey}`
+            );
+    
+            const data = response.data.values || [];  // Ensure 'data' is not undefined
+    
+            setDialougeInfo({
+                person: data[0] ? data[0][0] : "",        // B10 -> youtube_url
+                bookURL: data[0] ? data[0][1] : "",      // B11 -> twitter_url
+                chapter: data[0] ? data[0][2] : "",      // B11 -> twitter_url
+            });
+        };
+        fetchDialouge();
+    }, []);
+
 
 
     return (
@@ -42,6 +67,10 @@ export function ChapterPage({ book, chapter, setSelectedChapter, setSelectedBook
             <h1 className="title">
                 The book of {book.urlName} chapter {chapter}
             </h1>
+
+            <p>{dialougeInfo.person}</p>
+            <p>{dialougeInfo.bookURL}</p>
+            <p>{dialougeInfo.chapter}</p>
 
 
             <div className="nextButton-container">
